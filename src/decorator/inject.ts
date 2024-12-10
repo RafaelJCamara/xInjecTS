3debf0abf51eec54ff1c8cd19a58e2f26ea6e8c8
Rafael Camara <rafaelcamarac@gmail.com>
2024-12-10 11:05:04 +0100

    feat: add tokenless  dependency injection resolution for non-interface components

--- src/decorator/inject.ts
+++ src/decorator/inject.ts
@@ -1,18 +1,26 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { xContainer } from "../container/di-container";
-import 'reflect-metadata';
+import "reflect-metadata";
 
-export function Inject(identifier: string) {
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export function Inject(identifier?: string) {
   return function (target: any, propertyKey?: string | symbol, parameterIndex?: number) {
     if (typeof parameterIndex === "number") {
-      ApplyConstructorInjection(identifier, target, parameterIndex);
+      const resolvedIdentifier = identifier || resolveFromConstructor(target, parameterIndex);
+      ApplyConstructorInjection(resolvedIdentifier, target, parameterIndex);
     } else {
-      ApplyPropertyInjection(identifier, target, propertyKey!);
+      const resolvedIdentifier = identifier || resolveIdentifierFromType(target, propertyKey);
+      ApplyPropertyInjection(resolvedIdentifier, target, propertyKey!);
     }
   };
 }
 
-function ApplyPropertyInjection(identifier: string, target: any, propertyKey: string | symbol){
+function ApplyConstructorInjection(identifier: string, target: any, parameterIndex: number) {
+  const existingInjectedParams = Reflect.getMetadata("design:inject", target) || [];
+  existingInjectedParams[parameterIndex] = identifier;
+  Reflect.defineMetadata("design:inject", existingInjectedParams, target);
+}
+
+function ApplyPropertyInjection(identifier: string, target: any, propertyKey: string | symbol) {
   Object.defineProperty(target, propertyKey, {
     get: () => xContainer.resolve(identifier),
     enumerable: true,
@@ -20,8 +28,22 @@ function ApplyPropertyInjection(identifier: string, target: any, propertyKey: st
   });
 }
 
-function ApplyConstructorInjection(identifier: string, target: any, parameterIndex: number){
-  const existingInjectedParams = Reflect.getMetadata('design:inject', target) || [];
-  existingInjectedParams[parameterIndex] = identifier;
-  Reflect.defineMetadata('design:inject', existingInjectedParams, target);
+
+function resolveIdentifierFromType(target: any, propertyKey?: string | symbol): string {
+  if (!propertyKey) {
+    throw new Error("Property key is required to resolve type metadata.");
+  }
+
+  const type = Reflect.getMetadata("design:type", target, propertyKey);
+  if (!type) {
+    throw new Error(`Type metadata for property ${String(propertyKey)} is not available.`);
+  }
+
+  return type.name;
+}
+
+
+function resolveFromConstructor(target: any, parameterIndex: number): string {
+  const type = Reflect.getMetadata("design:paramtypes", target)[parameterIndex];
+  return type.name;
 }
