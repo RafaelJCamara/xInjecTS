bba0f1ce3d084bd9982511820bd04523fabe3c6c
Rafael Camara <rafaelcamarac@gmail.com>
2025-01-14 17:28:55 +0100

    feat: extend exported types

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+export type { GenericConstructor, InjectionKey } from './_shared/types';
 export { Injectable } from './decorator/injectable';
 export { Inject } from './decorator/inject';
 export { xContainer } from './container/di-container';
 export { Lifetime } from './container/lifetime';
 export type { IInjectableConfiguration } from './_shared/injectable.configuration';
+export {IDependency} from './container/dependency';
